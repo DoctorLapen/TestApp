76e1848e1f790177fc26d4a7fff2ba0cc582b530
DoctorLapen <kostia1272@gmail.com>
2019-11-30 18:47:13 +0200

    add SnapeData and TestTable

---
+++ Assets/Scripts/JsonDataClasses/ShapeData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class ShapeData
+{
+    public string modifiedData;
+    public Vector3 coordinate;
+    public string name;
+    public int id;
+    public enum type
+    {
+        Red,
+        Green,
+        Blue
+    };
+}
---
+++ Assets/Scripts/JsonDataClasses/TestTable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+[Serializable]
+public class TestTable
+{
+    public string name;
+    public int id;
+    public string modifiedData;
+    public ShapeData[] data;
+}
+
