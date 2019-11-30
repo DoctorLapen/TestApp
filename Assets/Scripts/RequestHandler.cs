2680f81870c18e0e0854b8f3b88ecd801b50a105
DoctorLapen <kostia1272@gmail.com>
2019-11-30 21:13:43 +0200

    add RequestHandler ,WebServerEmulator and JsonFolder

---
+++ Assets/Scripts/RequestHandler.cs
@@ -0,0 +1,30 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+//Класс для отправления запросов на сервер
+public class RequestHandler : MonoBehaviour
+{   
+    //событие которое происходить когда данные с сервера загружены
+    public event UnityAction<string> DataDownloaded;
+    private void Start()
+    {
+        StartCoroutine(SendRequestsWithInterval());
+    }
+    private void SendGetRequest()
+    {
+        string data = WebServerEmulator.GetJsonData();
+        Debug.Log(data);
+        DataDownloaded?.Invoke(data);
+    }
+    // Корутина которая отправляет Get-запрос  каждые 10 секунд;
+    private IEnumerator SendRequestsWithInterval() {
+        while (true) {
+            SendGetRequest();
+            yield return new WaitForSeconds(10f);
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/WebServerEmulator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+//Класс который эмулирует работу Web-сервера
+namespace Assets.Scripts
+{
+    public static class WebServerEmulator
+    {
+        private static string[] jsonArr= new string [3];
+
+        static WebServerEmulator()
+        {
+            InitJsonArr();
+        }
+        /*
+         * Метод который инициализирует jsonArr
+         * Данными из json-файлов в папке JsonFolder
+        */
+        private static void InitJsonArr()
+        {
+            string path;
+            string data;
+            for (int i = 0; i < 3; i++) {
+                path = Path.Combine(Application.dataPath, $"JsonFolder/Test{i+1}.json");
+                data = File.ReadAllText(path);
+                jsonArr[i] = data;
+            }
+        }
+        //метод которыий возвращает случайную json-строку из jsonArr
+        public static string GetJsonData()
+        {
+            int randomInt = UnityEngine.Random.Range(0,3);
+            return jsonArr[randomInt];
+        }
+    }
+}
