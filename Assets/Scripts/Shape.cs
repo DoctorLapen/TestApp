5f07e58d15bab6602d3ad9022ed64cf6b0606cad
DoctorLapen <kostia1272@gmail.com>
2019-12-01 19:58:03 +0200

    add Shape

---
+++ Assets/Scripts/Shape.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Класс который показывает данные фигуры в редакторе
+public class Shape : MonoBehaviour
+{
+    [SerializeField]
+    private string modifiedData;
+    [SerializeField]
+    private Vector3 coordinate;
+    [SerializeField]
+    private int id;
+    [SerializeField]
+    private TypeEnum type;
+
+    //Метод для инициализации или обновления данных фигуры
+    public void ChangeData(ShapeData shapeData)
+    {
+        transform.name = shapeData.name;
+        modifiedData = shapeData.modifiedData;
+        coordinate = shapeData.coordinate;
+        id = shapeData.id;
+        type = shapeData.type; 
+    }
+}
