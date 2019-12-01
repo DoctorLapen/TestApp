a079b501b7d7627c7e42e981553d5384e49fc13d
DoctorLapen <kostia1272@gmail.com>
2019-12-01 20:01:03 +0200

    add ShapeSpawner

---
+++ Assets/Scripts/ShapeSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Класс для спауна фигур
+public class ShapeSpawner : MonoBehaviour
+{   //Массив префабов фигур
+    public GameObject[] ShapePrefab = new GameObject[5];
+
+    [SerializeField]
+    private ShapeMoveController moveController;
+
+    private void Start()
+    {
+    /*
+     *Спаун фигур созданых из префабов 
+     *и их добавление в массив ShapesAr класса ShapeMoveController
+    */
+        for ( int i = 0; i< 5; i++)
+        {
+            moveController.ShapesArr[i] = Instantiate(ShapePrefab[i], Vector3.zero,Quaternion.identity);
+        }       
+    }    
+}
