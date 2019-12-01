8a865187aa3dbe82ed6e9281ace04083b94bf4a8
DoctorLapen <kostia1272@gmail.com>
2019-12-01 21:47:17 +0200

    add real SendGetRequest to RequestHandler

--- Assets/Scripts/RequestHandler.cs
+++ Assets/Scripts/RequestHandler.cs
@@ -3,30 +3,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Networking;
 //Класс для отправления запросов на сервер
 public class RequestHandler : MonoBehaviour
 {   
     //Событие которое происходить когда данные с сервера загружены
     public event UnityAction<string> DataDownloaded;
 
+    [SerializeField]
+    private string url = "https://owncloud.lindenvalley.de/index.php/s/lXIIkptI7tKJRL8/download";
+
     private void Start()
     {
-       StartCoroutine(SendRequestsWithInterval());
+       StartCoroutine(SendRequestsWithInterval(url));
     }
 
-    private void SendGetRequest()
+    private IEnumerator SendGetRequest(string url)
     {
-        string data = WebServerEmulator.GetJsonData();
-        DataDownloaded?.Invoke(data);
-    }
+        var request = UnityWebRequest.Get(url);
+        yield return request.SendWebRequest();        
 
+        if (!request.isHttpError && !request.isNetworkError)
+        {
+            DataDownloaded?.Invoke(request.downloadHandler.text);
+        }
+        else
+        {
+            Debug.LogErrorFormat("error request [{0}, {1}]", url, request.error);
+            Debug.Log(null);
+        }
+        request.Dispose();
+    }
     // Корутина которая отправляет Get-запрос  каждые 10 секунд;
-    private IEnumerator SendRequestsWithInterval() {
+    private IEnumerator SendRequestsWithInterval(string url)
+    {
         while (true) {
-            SendGetRequest();
+            StartCoroutine(SendGetRequest(url));
             yield return new WaitForSeconds(10f);
         }
-    }
-
-    
+    }   
 }
